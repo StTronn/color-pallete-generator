da2e2a6ad2d41b44b0936454bd1c5bf6d242c99d
rishav <rishav.21m@gmail.com>
2020-09-15 01:03:41 +0530

    Updates

---
+++ precache-manifest.d302ad7dd03d0fed244f0b94371dcc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afed3ff070081b381ba410cbbeabccd5",
+    "url": "/color-pallete-generator/index.html"
+  },
+  {
+    "revision": "e433eecfb06cc1e55a66",
+    "url": "/color-pallete-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56ae6b12f506fbac8438",
+    "url": "/color-pallete-generator/static/js/2.dc57017a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-pallete-generator/static/js/2.dc57017a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e433eecfb06cc1e55a66",
+    "url": "/color-pallete-generator/static/js/main.63a773c1.chunk.js"
+  },
+  {
+    "revision": "a7b241d84f242b594be6",
+    "url": "/color-pallete-generator/static/js/runtime-main.5bae4cbb.js"
+  }
+]);
